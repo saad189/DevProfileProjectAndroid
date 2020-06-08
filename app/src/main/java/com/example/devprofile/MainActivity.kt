3bbfe38675996ec942a94931af19799e71e42338
mahm <mahm@ciklum.com>
2020-06-08 09:11:19 +0500

    Layout Added for Tablet

--- app/src/main/java/com/example/devprofile/MainActivity.kt
+++ app/src/main/java/com/example/devprofile/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.devprofile
 
+import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.graphics.drawable.RoundedBitmapDrawableFactory
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //Code for making the logo, rounded
+        val bitmap = BitmapFactory.decodeResource(resources,R.drawable.devslopesprofilelogo)
+        val roundedBitmap = RoundedBitmapDrawableFactory.create(resources,bitmap)
+        roundedBitmap.cornerRadius = 15f
+        //roundedBitmap.isCircular = true // will change it to a circle
+        logo.setImageDrawable(roundedBitmap)
     }
 }
